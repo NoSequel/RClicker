6fca08024e4bddcc9011714175bd6e6b8e3e91a9
Marius Raphael <MariusRaphael@web.de>
2021-07-07 18:25:27 +0200

    Code Cleanup and Some GUI changes

--- src/support/clipboard.rs
+++ src/support/clipboard.rs
@@ -4,14 +4,14 @@ use imgui::{ClipboardBackend, ImStr, ImString};
 pub struct ClipboardSupport(ClipboardContext);
 
 pub fn init() -> Option<ClipboardSupport> {
-    ClipboardContext::new().ok().map(ClipboardSupport)
+	ClipboardContext::new().ok().map(ClipboardSupport)
 }
 
 impl ClipboardBackend for ClipboardSupport {
-    fn get(&mut self) -> Option<ImString> {
-        self.0.get_contents().ok().map(|text| text.into())
-    }
-    fn set(&mut self, text: &ImStr) {
-        let _ = self.0.set_contents(text.to_str().to_owned());
-    }
+	fn get(&mut self) -> Option<ImString> {
+		self.0.get_contents().ok().map(|text| text.into())
+	}
+	fn set(&mut self, text: &ImStr) {
+		let _ = self.0.set_contents(text.to_str().to_owned());
+	}
 }
